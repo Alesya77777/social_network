aaefee140b20a4c944154868a3bdd2548c658752
Alesya <kyzya77777@gmail.com>
2021-11-23 16:55:35 +0300

    Разбила разметку на компоненты

---
+++ src/components/Profile.jsx
@@ -0,0 +1,13 @@
+const Profile = () => {
+  return (
+    <div className="content">
+      <img src="https://ichef.bbci.co.uk/news/976/cpsprodpb/10B31/production/_117310486_15.jpg" alt=""/>
+      <div>avatar</div>
+      <div>My post</div>
+      <div>Post 1</div>
+      <div>Post 2</div>
+    </div>
+  )
+};
+
+export default Profile;
