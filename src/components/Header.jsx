95a5de32dbd45ef6c13e7aee8520b4ccd065534a
Alesya <kyzya77777@gmail.com>
2021-11-23 17:48:51 +0300

    First commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+const Header = () => {
+  return (
+    <header className="header">
+      <img src="https://www.planetware.com/wpimages/2020/02/france-in-pictures-beautiful-places-to-photograph-eiffel-tower.jpg" alt="" />
+    </header>
+  )
+};
+
+export default Header;
