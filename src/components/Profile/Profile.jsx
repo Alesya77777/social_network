2f1396d6ae280bd313f31031bdaaf76666c5d353
Alesya <kyzya77777@gmail.com>
2021-11-24 15:23:28 +0300

    Добавила компоненту Диалог

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -3,7 +3,7 @@ import classes from './Profile.module.css';
 
 const Profile = () => {
   return (
-    <div className={classes.content}>
+    <div>
       <img src="https://www.planetware.com/wpimages/2020/02/france-in-pictures-beautiful-places-to-photograph-eiffel-tower.jpg" alt="" />
       <div>avatar + des</div>
       <MyPosts />
