ef808bfbb741a8ee6986cb5ea092b57563b86bf2
Alesya <kyzya77777@gmail.com>
2021-12-17 22:38:18 +0300

    Добавила инфорацию о пользователе

--- src/components/Profile/ProfileInfo/ProfileInfo.jsx
+++ src/components/Profile/ProfileInfo/ProfileInfo.jsx
@@ -20,21 +20,30 @@ const ProfileInfo = (props) => {
                 <span>Ищу работу</span>
 
               </div>
-            ) :   <span>Не ищу работу</span>}
-              {
+            ) : <span>Не ищу работу</span>}
+            {
 
               (props.profile.lookingForAJobDescription) ? (
-              <div> Описание работы: 
-                <span>{`  `}  {props.profile.aboutMe}</span>
-              </div>
-            ) :  <span>Не ищу работу</span>
-              }
+                <div> Описание работы:
+                  <span>{`  `}  {props.profile.aboutMe}</span>
+                </div>
+              ) : <span>Не ищу работу</span>
+            }
           </div>
           <div>{(props.profile.aboutMe) ?
             <span className={classes.personDescription}>Обо мне:
               <span> {props.profile.aboutMe}
               </span>
-            </span> :<></>}
+            </span> : <></>}
+          </div>
+          <div>
+            <h3>Социальные сети</h3>
+   
+               <div className={classes.wrapper}>
+              {props.profile.contacts.facebook ? <a className={classes.facebook} href={props.profile.contacts.facebook}> </a> : <></>}
+              {props.profile.contacts.instagram ? <a className={classes.instagram} href={props.profile.contacts.instagram}> </a> : <></>}
+              </div>
+            
           </div>
 
         </div>
