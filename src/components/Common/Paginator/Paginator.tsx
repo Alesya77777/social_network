9608cf35226a36afb253b8a2c2a257854a488ce6
Alesya <kyzya77777@gmail.com>
2022-02-23 23:30:56 +0300

    Начала переводить компоненнты на typescript

--- src/components/Common/Paginator/Paginator.tsx
+++ src/components/Common/Paginator/Paginator.tsx
@@ -1,19 +1,26 @@
 import React, { useState } from 'react';
 import classes from './Paginator.module.css';
 
+type PropsType = {
+  totalItemsCount: number,
+  pageSize: number,
+  currentPage: number,
+  onPageChanged: (pageNumber:number) => void,
+  portionSize: number,
+}
 
-const Paginator = ({ totalItemsCount, pageSize, currentPage, onPageChanged, portionSize = 25 }) => {
+const Paginator: React.FC<PropsType> = ({ totalItemsCount, pageSize, currentPage, onPageChanged, portionSize = 25 }) => {
 
   const pageCount = Math.ceil(totalItemsCount / pageSize);
-  let pages = [];
+  let pages: Array<number> = [];
   for (let i = 1; i <= pageCount; i++) {
     pages.push(i);
   }
   let portionCount = Math.ceil(pageCount / portionSize);
-  let [ portionNumber, setPortionNumber ] = useState(1);
+  let [portionNumber, setPortionNumber] = useState(1);
   let leftPortionPageNumber = (portionNumber - 1) * portionSize + 1;
   let rightPortionPageNumber = portionNumber * portionSize;
-  
+
   return (
     <div className={classes.nagination}>
       {portionNumber > 1 && <button onClick={() => { setPortionNumber(portionNumber - 1) }} >PREV</button>}
@@ -22,10 +29,11 @@ const Paginator = ({ totalItemsCount, pageSize, currentPage, onPageChanged, port
           .filter(p => p >= leftPortionPageNumber && p <= rightPortionPageNumber)
           .map((p) => {
             return <button className={currentPage === p && classes.selectedPage} type="button"
-            onClick={(e) => onPageChanged(p)} key={p.id} >{p}</button>})
+              onClick={(e) => onPageChanged(p)} >{p}</button>
+          })
       }
       {portionCount > portionNumber && <button onClick={() => { setPortionNumber(portionNumber + 1) }} >NEXT</button>}
-      
+
     </div>
   )
 
